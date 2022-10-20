54d3d5b5c5cd7ce2099dd64f4c02e661ce83dd24
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-10-20 05:27:47 -0400

    Adding styles changes

--- FRONTEND-REACT/presupuesto-design-react/src/App.js
+++ FRONTEND-REACT/presupuesto-design-react/src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Form from './components/Form';
 import './Styles/App.css';
+import EgresoContainer from './components/EgresoContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+      <EgresoContainer />
     </div>
   );
 }
---
+++ FRONTEND-REACT/presupuesto-design-react/src/components/EgresoContainer.jsx
@@ -0,0 +1,13 @@
+import Ingreso from './Ingreso'
+import Egreso from './Egreso'
+
+const EgresoContainer = () => {
+  return (
+    <div class="contenedor limpiarEstilos">
+        <Ingreso />
+        <Egreso />
+    </div>
+  )
+}
+
+export default EgresoContainer
