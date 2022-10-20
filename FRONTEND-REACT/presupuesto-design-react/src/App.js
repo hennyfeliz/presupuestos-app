0e9a04a7513a606f8c5c78764f60d7200dfb33e3
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-10-20 05:14:37 -0400

    adding header

--- FRONTEND-REACT/presupuesto-design-react/src/App.js
+++ FRONTEND-REACT/presupuesto-design-react/src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header';
 import './Styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Funciona</h1>
+      <Header />
     </div>
   );
 }
