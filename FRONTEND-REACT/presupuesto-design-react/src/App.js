41c96675228fb8bdda1a0ebae9b8c731e4e40366
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-10-20 05:16:08 -0400

    adding central form

--- FRONTEND-REACT/presupuesto-design-react/src/App.js
+++ FRONTEND-REACT/presupuesto-design-react/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Form from './components/Form';
 import './Styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Form />
     </div>
   );
 }
