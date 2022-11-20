6f4d3c97e93322de3c26485930b82fa773b0df3a
Henny <mingito.2017@gmail.com>
2022-11-20 04:32:06 -0400

    adding directory changes

--- BACKEND/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-mongoose.connect('mongodb://localhost/presupuesto-app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(db => console.log('db is connected'))
-.catch(err => console.log(err))
